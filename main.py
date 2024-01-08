9500caddd833f433202dd0f236665de377211b00
SuperAnt220 <zp-a@i.ua>
2024-01-08 22:06:47 +0200

    uuuhh trying to understand this

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-from pyflowlauncher import Plugin, Result, Method
-from pyflowlauncher.result import ResultResponse
+import sys
+from pathlib import Path
 
-plugin = Plugin()
+plugindir = Path.absolute(Path(__file__).parent)
+paths = (".", "lib", "plugin")
+sys.path = [str(plugindir / p) for p in paths] + sys.path
 
 
-class Query(Method):
+from flowlauncher import FlowLauncher
+import webbrowser
 
-    def __call__(self, query: str) -> ResultResponse:
-        r = Result(
-            Title="This is a title!",
-            SubTitle="This is the subtitle!"
-        )
-        self.add_result(r)
-        return self.return_results()
 
-plugin.add_method(Query())
-plugin.run()
+class HelloWorld(FlowLauncher):
+
+    def query(self, query):
+        return [
+            {
+                "Title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
+                "SubTitle": "This is where your subtitle goes, press enter to open Flow's url",
+                "IcoPath": "Images/app.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
+                }
+            }
+        ]
+
+    def context_menu(self, data):
+        return [
+            {
+                "Title": "Hello World Python's Context menu",
+                "SubTitle": "Press enter to open Flow the plugin's repo in GitHub",
+                "IcoPath": "Images/app.png",
+                "JsonRPCAction": {
+                    "method": "open_url",
+                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher.Plugin.HelloWorldPython"]
+                }
+            }
+        ]
+
+    def open_url(self, url):
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    HelloWorld()
